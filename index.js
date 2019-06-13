cd027c94d30cf0aa48aef9bbc73ea7c3c9d3485c
Sara Walker <sarawall1187@gmail.com>
2019-06-13 20:21:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,36 +23,11 @@ function addNewLiOnClick(){
   let button = document.querySelector('input[value="Add Employee"]')
   button.addEventListener("click", addNewElementAsLi());
   document.querySelector('form').reset()
-  //  const input = document.querySelector('input');
-  //  input.addEventListener('click', function(e) {
-  //    console.log(input) }
-  //     return input.addNewElementAsLi();
-  //     return input.value = '';
-  //  })
 }
 
 function clearEmployeeListOnLinkClick(){
   document.querySelector('a').addEventListener("click", function(){
    let list = document.getElementsByClassName('employee-list')[0]
-   // while (list.firstElementChild){
-   //   list.removeChild(list.firstElementChild)
-   // }
    list.innerHTML = ''
- });
-  // let li = document.querySelector('li')
-  // let button = document.querySelector('a')
-  // button.addEventListener('click' function(e){
-  //   $(li).each(function(){
-  //       return li.remove();
-  //     })
-  // })
-
-  // let button = document.querySelector('a')
-  // button.addEventListener('click', function(e) {
-  //   ul.remove();
-  // }
-  // return addNewLiOnClick().removeEventListener('click' function(e){
-  //   let ul = document.querySelector('ul');
-    // return ul.remove();
-  // })
+  });
 }
