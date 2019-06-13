4101b7919fb7bfa82d69d98a1abda17956fb2103
Sara Walker <sarawall1187@gmail.com>
2019-06-13 17:00:05 +0000

    Done.

--- index.js
+++ index.js
@@ -3,5 +3,34 @@ function preventRefreshOnSubmit(){
       event.preventDefault()
     })
 }
-
 preventRefreshOnSubmit()
+
+
+function retrieveEmployeeInformation(){
+  const input = document.querySelector('input');
+  return input.value;
+}
+
+function addNewElementAsLi(){
+  let ul = document.querySelector('ul');
+  let li = document.createElement('li');
+  const info = retrieveEmployeeInformation()
+  li.innerHTML = info.toString();
+  ul.appendChild(li)
+}
+
+function addNewLiOnClick(){
+   const input = document.querySelector('input');
+   input.addEventListener('click', function(e) {
+      return input.addNewElementAsLi();
+      return input.value = '';
+   })
+}
+
+function clearEmployeeListOnLinkClick(){
+  let ul = document.querySelector('ul')
+  let button = document.querySelector('a')
+  button.addEventListener('click', function(e) {
+    ul.remove();
+  }
+}
